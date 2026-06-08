75350288b89bafa2db37c66a77e0cb6ce4f10632
lilianlacerda <lilianraiany@gmail.com>
2026-06-08 10:24:36 -0300

    criação da classe model categoria

---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(max = 50, message = "O atributo nome precisa ter no máximo 50 caracteres!")
+	private String nome;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
