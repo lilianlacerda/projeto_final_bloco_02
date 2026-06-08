c246bb9807709626a83af929eaae470c0dfa180f
lilianlacerda <lilianraiany@gmail.com>
2026-06-08 12:12:39 -0300

    criação do model de produto

---
+++ src/main/java/com/generation/projeto_final_bloco_02/model/Produto.java
@@ -0,0 +1,81 @@
+package com.generation.projeto_final_bloco_02.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "o atributo nome é obrigatório!")
+	@Size(max = 100, message = "O atributo nome só pode ter no máximo 100 caracteres!")
+	private String nome;
+	
+	@Size(max = 50, message = "O atributo laboratório só pode ter no máximo 50 caracteres!")
+	private String laboratorio;
+	
+	@NotNull(message = "o atributo preço não pode ser nulo!")
+	private BigDecimal preco;
+	
+	@NotNull(message = "O atributo quantidade não pode ser nulo!")
+	private int quantidade;
+	
+	private LocalDate dataValidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LocalDate getDataValidade() {
+		return dataValidade;
+	}
+
+	public void setDataValidade(LocalDate dataValidade) {
+		this.dataValidade = dataValidade;
+	}
+}
